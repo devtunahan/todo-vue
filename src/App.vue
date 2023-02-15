b2c9b7edcf5207c88c114a511f5cdf890bf6ba71
tunahan <bstunahan@gmail.com>
2023-02-15 14:50:17 +0100

    tailwind, todos, delete todos

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-gray-600 text-white w-1/2 m-auto mt-12">
+    <div class="text-center border-b-2 border-black py-4">
+      <h1 class="text-3xl py-4"> Unsere Todos</h1>
+      <p class="text-xl">Offene Todos: {{ todos.length }} </p>
+    </div>
+    <div v-for="(todo,index) in todos" :key="todo.todo">
+      <TodoItem :todoprop="todo" :todoIndex="index" @toggledone-index="toggleDone" @removeTodo-index="deleteTodo"/>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from './components/TodoItem.vue'
 
 export default {
   name: 'App',
+  methods: {
+    toggleDone(index) {
+      this.todos[index].done = !this.todos[index].done 
+    },
+    deleteTodo(index) {
+      this.todos.splice(index,1)
+    }
+  },
+  data() {
+    return {
+      todos:[
+        {todo: "einkaufen", done: false},
+        {todo: "sport", done: false},
+        {todo: "programmieren", done: true},
+        {todo: "lesen", done: false},
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    TodoItem
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex justify-between items-center py-2 px-2" 
+  :class="{'bg-green-600': this.todo.done, 'bg-red-600': !this.todo.done}"
+  >
+    <h1 class="cursor-pointer" @click="toggleTodo">{{ todo.todo }}</h1>
+    <button class="bg-black py-1 px-1 rounded" @click="removeTodo">delete todo</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: [
+    "todoprop",
+    "todoIndex"
+  ],
+
+  data() {
+      return {
+        todo: this.todoprop
+      }
+    },
+  methods: {
+    toggleTodo() {
+      this.$emit("toggledone-index",this.todoIndex);
+    },
+    removeTodo() {
+      this.$emit("removeTodo-index",this.todoIndex);
+    }
+  }
+};
+</script>
+
